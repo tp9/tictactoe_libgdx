68b225d9696d82e7e4f78eeb47f81021f1e14aba
tp9 <theprofessional9@gmail.com>
2014-07-07 17:52:47 -1000

    Created GameScreen and main class

---
+++ core/src/com/tp9/tictactoe/GameScreen.java
@@ -0,0 +1,55 @@
+package com.tp9.tictactoe;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+	
+	final TicTacToe game;
+
+	public GameScreen(final TicTacToe gam) {
+		game = gam;
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
