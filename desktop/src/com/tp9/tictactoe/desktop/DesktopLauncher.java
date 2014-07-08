b3c316feb311db5ea1b703866311ff2b4b6798fc
tp9 <theprofessional9@gmail.com>
2014-07-07 16:24:52 -1000

    First commit

---
+++ desktop/src/com/tp9/tictactoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tp9.tictactoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tp9.tictactoe.TicTacToe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToe(), config);
+	}
+}
